7d12def5b486488050dc9e3d376f228a44f1b64d
Sophonias Isaac <sophonias8@gmail.com>
2019-09-15 22:11:39 +0100

    Update DestructuringArrays.js

--- ES6/DestructuringArrays.js
+++ ES6/DestructuringArrays.js
@@ -1,6 +1,6 @@
-Using destructuring assignment to swap the values of carDoor1 and carDoor2 so that carDoor1 receives the value stored in carDoor2, and carDoor2 receives the value stored in carDoor1.
+// Using destructuring assignment to swap the values of carDoor1 and carDoor2 so that carDoor1 receives the value stored in carDoor2, and carDoor2 receives the value stored in carDoor1.
 
- Since the strict mode is applied inside the function, don’t try to use const because you will get reference error, because b is not defined inside the function. 
+//  Since the strict mode is applied inside the function, don’t try to use const because you will get reference error, because b is not defined inside the function. 
 
 
 let carDoor1 = 8, carDoor2 = 6;
@@ -8,8 +8,7 @@ let carDoor1 = 8, carDoor2 = 6;
   "use strict";
  
     [carDoor2,carDoor1] = [carDoor1,carDoor2];
-    //Using destructuring assignment to swap the values of 
-    //carDoor1 and carDoor2 so that carDoor1 receives the value stored in carDoor2, and carDoor2 receives the value stored in carDoor1.
+   
  
 })();
 console.log(carDoor1); // should be 6
