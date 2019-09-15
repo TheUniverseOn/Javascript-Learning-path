d17ae9d41e6d50d5f91ecf0d7b4b4ce5199d6f72
Sophonias Isaac <sophonias8@gmail.com>
2019-09-15 19:22:29 +0100

    Add files via upload
    One of the newly introduced ES6 spread operator allows arrays to be expanded and other expressions where multiple parameters and elements are needed. The spread operator only works in-place, like in an argument to a function or in an array literal.

---
+++ ES6/spreadOperator.js
@@ -0,0 +1,7 @@
+const mentalModels = ['Probabilistic Thinking', 'Inversion', 'Compounding',  ' Power Laws', "Pareto Principle" ];
+let mentalModel2; 
+(function(){
+    
+    mentalModels2 = [...mentalModels]
+})(); 
+console.log(mentalModels2);
