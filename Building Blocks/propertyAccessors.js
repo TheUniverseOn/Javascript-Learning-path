e17718cd5cbf8df11cd6343de6dd05ec1c0b918a
Sophonias Isaac <sophonias8@gmail.com>
2019-09-16 19:27:30 +0100

    Create propertyAccessors.js

---
+++ Building Blocks/propertyAccessors.js
@@ -0,0 +1,29 @@
+var person = {}; 
+person{'firstname'} = 'Zozo'; 
+person{'lastname'} = 'Ora'; 
+console.log(firstname) //  'Zozo'; 
+
+person = {'firstname': 'Zozo', lastname: 'Ora'}
+
+console.log(person['firstname']); 
+
+// expected output: "Zozo"
+
+
+
+var incubator = {};
+incubator['companyname']  = 'Y-Combinator'; 
+incubator['CEO'] = 'Michael Seibel'; 
+
+console.log(incubator.companyname); // Y-Combinator
+
+const firm = {"vcname": "A16Z", "location": "California"}
+
+console.log(firm["vcname"] //A16Z
+
+
+
+
+/*One can think of an object as an associative array(a.k.a map, dictionary, hash, lookup table)
+The keys in this array are the names of the object's properties /  
+There are two ways to access properties: dot notation and bracket notation */
