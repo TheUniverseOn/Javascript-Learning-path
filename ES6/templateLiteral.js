7500315b92b1af685a423e81820bf0045ad45bcd
Sophonias Isaac <sophonias8@gmail.com>
2019-09-16 14:52:51 +0100

    Update templateLiteral.js

--- ES6/templateLiteral.js
+++ ES6/templateLiteral.js
@@ -16,3 +16,14 @@ console.log(greeting);
 
 //Hallo,ich heiße Haribo bobo! I am currently studying German  
 as you can see and also using literal template. I am also 200 years old LOL ;) 
+
+
+
+Here we have done quite a lot of things firstly using backticks(`), not qoutes (", ')to be able wrap a string. 
+
+next, if you have a look the string is multi line both in the actual code and also in the output. This means no more 
+\n within strings which can be come messy. 
+
+The ${variable} syntax used above is a placeholder which means I don't have to use concatenation with the + operator anymore. 
+Hurray!. To add variables to strings, you just  insert the variable in a template string and wrap it up with ${ and }. Wait, the
+template strings wonder has not finshe it. I  can also add other expressions in the string literal i.e ${name + age}. 
