c0324d78d792f795d532624651bdf096fedac517
Sophonias Isaac <sophonias8@gmail.com>
2019-09-17 09:50:28 +0100

    Update propertyAccessors.js

--- Building Blocks/propertyAccessors.js
+++ Building Blocks/propertyAccessors.js
@@ -59,3 +59,25 @@ console.log(Industries["IndustryName"]);
 /*One can think of an object as an associative array(a.k.a map, dictionary, hash, lookup table)
 The keys in this array are the names of the object's properties /  
 There are two ways to access properties: dot notation and bracket notation */
+
+
+
+
+/* Another use of bracket notation on objects is to access a property 
+which is stored as the value of a variable. This can be very useful for 
+iterating through an object's properties or when accessing a lookup table.
+
+Here is an example of using a variable to access a property:
+Note that we do not use quotes around the variable name when using it to access the property
+because we are using the value of the variable, not the name.
+
+*/
+
+
+
+  var gadgets = 
+  {  Imac: "apple", phone: "Samsung", Philipd: "TV"}; 
+
+  var device = "Imac"; 
+  var Brand = gadgets[device]; 
+  console.log(Brand); //apple
