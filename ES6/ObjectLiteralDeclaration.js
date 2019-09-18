bd4b3abf60e8ba16af1ce8c274d2c74f092d4c5c
Sophonias Isaac <sophonias8@gmail.com>
2019-09-18 16:04:00 +0100

    Create ObjectLiteralDeclaration.js

---
+++ ES6/ObjectLiteralDeclaration.js
@@ -0,0 +1,10 @@
+ const createRobot = (name, age, gender) => {
+    "use strict";
+    
+    return {name, age, gender};
+    // change code above this line
+  };
+  console.log(createRobot("sophonias", 23, "male")); //{ name: 'sophonias', age: 23, gender: 'male' } 
+  
+  ES6 provides the syntactic easy way to eliminate the redudancy of having to code x: x. you can simply write x once, it will be 
+  converted x: x 
