444f4d5b2eb11a8751589e809fe20b03f54d0f15
Sophonias Isaac <sophonias8@gmail.com>
2019-09-20 16:39:19 +0100

    Add files via upload

---
+++ Object Oriented Programming/Object.js
@@ -0,0 +1,20 @@
+/* If you think about the things people use everyday, like cars, shops, and 
+shoes. These are all objects and they are tangibale
+
+What kind of qualities do they have? A car has wheels, Shops sell items, shoes have materials
+
+These qualities are properties, define what makes up an object. Note that 
+Similar objects share the same properties, but may have different values for those properties. For example, all vehicles have wheels 
+but not all vehicles the same number of wheels. 
+
+Objects in javascript are used to model-world objects, giving them properties and behavior just like their 
+real-world counterparts. Here's an example using these concepts to create 
+a goat object
+ */
+
+ let goat = {
+     name: "fredo", 
+     numLegs = 4
+ }; 
+//  The goat object has two property/value pairs: a name of "fredo" and 
+numLegsof 4. 
