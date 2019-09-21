cdfedffebcdce2efa2f962d2c941cc3a4bde9ce5
Sophonias Isaac <sophonias8@gmail.com>
2019-09-21 15:32:26 +0100

    Update ExtractMatches.js

--- Regular Expressions/ExtractMatches.js
+++ Regular Expressions/ExtractMatches.js
@@ -1,5 +1,5 @@
 We can also also actract patterns from strings using .match()
 
 let extractor = "Extract the word 'coding' from this string.";
-let Regex = /coding/; // Change this line
-let result = extractor.match(Regex); // Change this line
+let Regex = /coding/; 
+let result = extractor.match(Regex); 
