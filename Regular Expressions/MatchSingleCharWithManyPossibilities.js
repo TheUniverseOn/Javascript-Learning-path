d14645e3bec2f03bf51ac1610bcdc3003cd40bae
Sophonias Isaac <sophonias8@gmail.com>
2019-09-21 16:16:22 +0100

    Update MatchSingleCharWithManyPossibilities.js

--- Regular Expressions/MatchSingleCharWithManyPossibilities.js
+++ Regular Expressions/MatchSingleCharWithManyPossibilities.js
@@ -5,5 +5,31 @@
 
 
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
-let vowelRegex = /[aeiou]/gi; // Change this line
-let result = quoteSample.match(vowelRegex); // Change this line
+let vowelRegex = /[aeiou]/gi;
+let result = quoteSample.match(vowelRegex); 
+// print: 
+[ 'e', 
+  'a', 
+  'e', 
+  'o', 
+  'u', 
+  'i', 
+  'e', 
+  'a', 
+  'o', 
+  'e', 
+  'o', 
+  'e', 
+  'I', 
+  'a', 
+  'e', 
+  'o', 
+  'o', 
+  'e', 
+  'i', 
+  'o', 
+  'e', 
+  'o', 
+  'i', 
+  'e', 
+  'i' ] 
