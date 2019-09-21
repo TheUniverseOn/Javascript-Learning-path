99482555ddd6f48dfef0f4c72a7e2b15fac9934d
Sophonias Isaac <sophonias8@gmail.com>
2019-09-21 15:45:28 +0100

    Update ExtractMatches.js

--- Regular Expressions/ExtractMatches.js
+++ Regular Expressions/ExtractMatches.js
@@ -1,5 +1,10 @@
-We can also also actract patterns from strings using .match()
+//We can also also Extract patterns from strings using .match()
 
 let extractor = "Extract the word 'coding' from this string.";
-let Regex = /coding/; 
-let result = extractor.match(Regex); 
+let Regex = /coding/gi; 
+console.log(extractor.match(Regex)); //[ 'coding' ] ​​
+
+
+let twinkleFinder = "Twinkle, twinkle, little star";
+let Regex = /Twinkle/gi; 
+console.log(twinkleFinder.match(Regex));  // console.log(twinkleFinder.match(Regex)); 
