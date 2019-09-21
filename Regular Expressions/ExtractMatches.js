8decd01d9ee36b6554a1cea260d0ef265d0660d3
Sophonias Isaac <sophonias8@gmail.com>
2019-09-21 18:25:06 +0100

    Update ExtractMatches.js

--- Regular Expressions/ExtractMatches.js
+++ Regular Expressions/ExtractMatches.js
@@ -8,3 +8,35 @@ console.log(extractor.match(Regex)); //[ 'coding' ] ​​
 let twinkleFinder = "Twinkle, twinkle, little star";
 let Regex = /Twinkle/gi; 
 console.log(twinkleFinder.match(Regex));  // console.log(twinkleFinder.match(Regex)); 
+
+
+
+//Inside a character set, you can define a range of characters to match using a hyphencharacter: -.
+
+//For example, to match lowercase letters athrough eyou would use [a-e].
+let extractor= "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[a-e]/gi;
+console.log(extractor.match(vowelRegex)); //
+ 
+[ 'B', 
+  'e', 
+  'a', 
+  'e', 
+  'b', 
+  'e', 
+  'a', 
+  'b', 
+  'e', 
+  'c', 
+  'd', 
+  'e', 
+  'a', 
+  'e', 
+  'e', 
+  'd', 
+  'c', 
+  'e', 
+  'c', 
+  'e', 
+  'd' ] 
+
