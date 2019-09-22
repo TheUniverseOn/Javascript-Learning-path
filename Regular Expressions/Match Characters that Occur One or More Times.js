17d09a8b2312bdbe836eab8eb648561601b35ecf
Sophonias Isaac <sophonias8@gmail.com>
2019-09-22 14:55:52 +0100

    Update Match Characters that Occur One or More Times.js

--- Regular Expressions/Match Characters that Occur One or More Times.js
+++ Regular Expressions/Match Characters that Occur One or More Times.js
@@ -1,3 +1,4 @@
+/*For example, /a+/gwould find one match in "abc"and return ["a"]. Because of the +, it would also find a single match in "aabc"and return ["aa"]. */
 let name = "Sophonias";
 let Regex = /s+/gi; // 
 let result = name.match(Regex); // [ 'S', 's' ] 
