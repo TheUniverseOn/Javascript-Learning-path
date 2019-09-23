87429beee12f0269ee575770159b2684835dc798
Sophonias Isaac <sophonias8@gmail.com>
2019-09-23 14:19:06 +0100

    Create split().js

---
+++ String Methods/split().js
@@ -0,0 +1,45 @@
+//split a string into substrings using the specified separator and return them and array 
+
+
+var glue = "Glue is here From Switzerland."
+console.log(glue.split(' ')); // [ 'Glue', 'is', 'here', 'From', 'Switzerland.' ]
+
+
+var glue = "Glue is here From Switzerland."
+console.log(glue.split( , )); // [ 'Glue', 'is', 'here', 'From', 'Switzerland.' ] 
+
+
+//spliting each separator
+var glue = "Glue is here From Switzerland."
+console.log(glue.split( '' )) //  
+[ 'G', 
+  'l', 
+  'u', 
+  'e', 
+  ' ', 
+  'i', 
+  's', 
+  ' ', 
+  'h', 
+  'e', 
+  'r', 
+  'e', 
+  ' ', 
+  'F', 
+  'r', 
+  'o', 
+  'm', 
+  ' ', 
+  'S', 
+  'w', 
+  'i', 
+  't', 
+  'z', 
+  'e', 
+  'r', 
+  'l', 
+  'a', 
+  'n', 
+  'd', 
+  '.' ] 
+  
