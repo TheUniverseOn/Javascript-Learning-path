0bb3e07f34ee7d911c7bdcd0fc03677e9e2d944d
Sophonias Isaac <sophonias8@gmail.com>
2019-09-23 13:33:33 +0100

    Create repeat().js

---
+++ String Methods/repeat().js
@@ -0,0 +1,5 @@
+var glue = "Glue is here form Switzerland. ";
+console.log( glue.repeat("3")); 
+//Glue is here form Switzerland. 
+//Glue is here form Switzerland. 
+//Glue is here form Switzerland.  
