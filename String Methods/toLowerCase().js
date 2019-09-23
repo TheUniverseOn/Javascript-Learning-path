e15b2a634954a26359006ea5683d231ccf48dcfe
Sophonias Isaac <sophonias8@gmail.com>
2019-09-23 13:48:42 +0100

    Create toLowerCase().js

---
+++ String Methods/toLowerCase().js
@@ -0,0 +1,5 @@
+//Converts all the alphabetic characters in a string to lowercase.
+
+var glue = "GLUE IS HERE FROM SWITZERLAND. "
+ console.log(glue.toLowerCase())
+//glue is here from switzerland.  
