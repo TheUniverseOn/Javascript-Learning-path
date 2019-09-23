2e99d77c2eba7d7e8327d1a86535d51b0f8d5444
Sophonias Isaac <sophonias8@gmail.com>
2019-09-23 13:44:58 +0100

    Create trim().js

---
+++ String Methods/trim().js
@@ -0,0 +1,5 @@
+//Removes the leading and trailing white space and line terminator characters from a string.
+
+var glue = "Glue is    here From Switzerland      .    "
+let result = glue.trim();
+console.log(result) // Glue is here From Switzerland. 
