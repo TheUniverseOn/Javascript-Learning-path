0e825856a9a3f35bdb4d0c95b979266d99a2fcd0
Sophonias Isaac <sophonias8@gmail.com>
2019-09-23 13:51:16 +0100

    Create search().js

---
+++ String Methods/search().js
@@ -0,0 +1,7 @@
+//Finds the first substring match in a regular expression search.
+var glue = "Glue is here From Switzerland."
+console.log(glue.search('e')) //e
+
+
+var glue = "Glue is here From Switzerland."
+console.log(glue.search('is')) //5
