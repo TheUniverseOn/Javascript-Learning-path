73b1b01e7b235ff7d74c35d6cca4cbc30e42cdb3
Sophonias Isaac <sophonias8@gmail.com>
2019-09-24 10:47:50 +0100

    Create push().js

---
+++ Array Methods/push().js
@@ -0,0 +1,12 @@
+//Add an Array to the end of the array 
+var fruits = [ 'Apple', 'Banana', 'kiwi', 'Mango', 'Pineapple', 'Grapes'] 
+var newFruits = fruits.push('strawberry');
+console.log(fruits)
+/*
+[ 'Apple', 
+'Banana', 
+'kiwi', 
+'Mango', 
+'Pineapple', 
+'Grapes', 
+'strawberry' ]  */
