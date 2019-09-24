eb3f8850cfda0ffb8e4b6fb11b794b9fb00d503d
Sophonias Isaac <sophonias8@gmail.com>
2019-09-24 11:01:39 +0100

    Create splice().js

---
+++ Array Methods/splice().js
@@ -0,0 +1,5 @@
+var fruits = [ 'blackberry', 'Banana', 'kiwi', 'Mango', 'Pineapple', 'Grapes' ];
+var removedItem = fruits.splice(0, 3,) 
+console.log(removedItem) //[ 'blackberry', 'Banana', 'kiwi' ] 
+
+console.log(fruits)[ 'Mango', 'Pineapple', 'Grapes' ] 
