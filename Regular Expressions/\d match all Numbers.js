97026c815c3ec9d4f6764a16a5c721eb3052df0f
Sophonias Isaac <sophonias8@gmail.com>
2019-09-24 19:34:32 +0100

    Update \d match all Numbers.js

--- Regular Expressions/\d match all Numbers.js
+++ Regular Expressions/\d match all Numbers.js
@@ -5,3 +5,47 @@
 let numString = "Your sandwich will be $5.00";
 let numRegex = /\d/g; // Change this line
 console.log( numString.match(numRegex).length); //3
+
+
+
+// You can also search for non-digits using a similar shortcut that uses an uppercase Dinstead.
+
+// The shortcut to look for non-digit characters is \D. 
+// This is equal to the character class [^0-9], which looks for a single character that is not a number
+// between zero and nine.
+
+let numString = "Your sandwich will be $5.00";
+let numRegex = /\D/g; // Change this line
+console.log( numString.match(numRegex).length); //24 
+
+
+
+ let numString = "Your sandwich will be $5.00";
+let numRegex = /\D/g; // Change this line
+console.log( numString.match(numRegex)); 
+
+//
+[ 'Y', 
+  'o', 
+  'u', 
+  'r', 
+  ' ', 
+  's', 
+  'a', 
+  'n', 
+  'd', 
+  'w', 
+  'i', 
+  'c', 
+  'h', 
+  ' ', 
+  'w', 
+  'i', 
+  'l', 
+  'l', 
+  ' ', 
+  'b', 
+  'e', 
+  ' ', 
+  '$', 
+  '.' ]
