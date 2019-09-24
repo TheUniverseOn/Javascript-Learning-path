ad664695b2291f712642fdb3d07731b163220d4b
Sophonias Isaac <sophonias8@gmail.com>
2019-09-24 10:49:56 +0100

    Create pop().js

---
+++ Array Methods/pop().js
@@ -0,0 +1,4 @@
+//remove array the end of the array 
+var fruits = [ 'Apple', 'Banana', 'kiwi', 'Mango', 'Pineapple', 'Grapes', 'strawberry' ] 
+var removedFruit = fruits.pop(); 
+console.log(removedFruit) // strawberry
