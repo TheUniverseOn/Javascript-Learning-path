7f1f637b6d958303d722835e5eee58cf931fb2e4
Sophonias Isaac <sophonias8@gmail.com>
2019-09-24 10:51:30 +0100

    Create unshift().js

---
+++ Array Methods/unshift().js
@@ -0,0 +1,4 @@
+//add to the front of an array 
+var fruits = [ 'Apple', 'Banana', 'kiwi', 'Mango', 'Pineapple', 'Grapes'] 
+var addFront = fruits.unshift('blackberry');
+console.log(fruits) /* [ 'blackberry', 'Banana', 'kiwi', 'Mango', 'Pineapple', 'Grapes' ]*/
