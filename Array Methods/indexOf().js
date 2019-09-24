1605b9c3659d68463ab4f96a2f718c3f1fbb973e
Sophonias Isaac <sophonias8@gmail.com>
2019-09-24 10:54:56 +0100

    Create indexOf().js

---
+++ Array Methods/indexOf().js
@@ -0,0 +1,5 @@
+//find the index of an item in the array 
+
+var fruits =  [ 'blackberry', 'Banana', 'kiwi', 'Mango', 'Pineapple', 'Grapes' ]*/
+var pos = fruits.indexOf('Banana'); 
+console.log(pos)//1
