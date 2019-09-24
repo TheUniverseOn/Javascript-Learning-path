dbdaa0dd6b448fe104beef50011e1a3985c627e0
Sophonias Isaac <sophonias8@gmail.com>
2019-09-24 10:50:47 +0100

    Create shift().js

---
+++ Array Methods/shift().js
@@ -0,0 +1,4 @@
+//remove from the first array 
+var fruits = [ 'Apple', 'Banana', 'kiwi', 'Mango', 'Pineapple', 'Grapes', 'strawberry'] 
+var removeFirst = fruits.shift(); 
+console.log(removeFirst) //Apple 
