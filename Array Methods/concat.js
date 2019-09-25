cece7244bf275c4d1479510ea3fb69f53e29d74f
Sophonias Isaac <sophonias8@gmail.com>
2019-09-25 17:46:47 +0100

    Create concat.js

---
+++ Array Methods/concat.js
@@ -0,0 +1,16 @@
+
+//The concat() method is used to merge two or more arrays. This method does not change the existing arrays,
+//but instead returns a new array.
+
+
+
+function connect(value, value2){
+    return value.concat(value2)
+}
+concated = value = ['Hey I am an entrepreneur'].concat(value2 = ['I am also a new Developer']); 
+console.log(concated) //[ 'Hey I am an entrepreneur', 'I am also a new Developer' ] 
+
+
+var hege = ["Cecilie", "Lone"];
+var stale = ["Emil", "Tobias", "Linus"];
+console.log(children = hege.concat(stale)); // [ 'Cecilie', 'Lone', 'Emil', 'Tobias', 'Linus' ] 
