d468e769f8592d9e4c1dbb24e3891f87bb7c3672
Sophonias Isaac <sophonias8@gmail.com>
2019-09-25 18:01:45 +0100

    Create fill.js

---
+++ Array Methods/fill.js
@@ -0,0 +1,6 @@
+The fill() methods fills(modifies) all the elements of an array from start index(0) to an end index(array length). 
+It turns the modified array. 
+
+var array = [4, 5, 6, 7, 8]; 
+console.log(array.fill('hey, i am fill',2,5 ))
+//[ 4, 5, 'hey, i am fill', 'hey, i am fill', 'hey, i am fill' ]
