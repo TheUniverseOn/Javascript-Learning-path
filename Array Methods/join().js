99642c6cbf07124dcca6ad33232c3d9fd995e486
Sophonias Isaac <sophonias8@gmail.com>
2019-09-26 14:30:24 +0100

    Add files via upload

---
+++ Array Methods/join().js
@@ -0,0 +1,9 @@
+// The join() method creates and returns a new string by concatenating all of the elements in an array (or an array-like object), separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator.
+
+function Albums(){
+
+}
+
+
+var joinTheAlbums = ['When I was 19', 'techno', 'worlder'].join(',');
+console.log(joinTheAlbums); 
