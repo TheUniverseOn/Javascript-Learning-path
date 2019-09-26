ec30da6df4c4871e6a1de48cb547ba5379748db8
Sophonias Isaac <sophonias8@gmail.com>
2019-09-26 14:10:20 +0100

    Add files via upload

--- Array Methods/includes().js
+++ Array Methods/includes().js
@@ -1,2 +1,4 @@
+// The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+
 var arr1 = [1, 2, [3, 4]];
 console.log(arr1.includes(1)); 
