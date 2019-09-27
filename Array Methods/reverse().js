c61d4acd9f8dc7cd199b75660269f29ad1694884
Sophonias Isaac <sophonias8@gmail.com>
2019-09-27 22:59:54 +0100

    Create reverse().js

---
+++ Array Methods/reverse().js
@@ -0,0 +1,14 @@
+//The reverse() method reverses an array in place. The first array element becomes the last, and the last array element becomes the first.//
+
+var array1 = ['one', 'two', 'three'];
+console.log('array1: ', array1);
+// expected output: Array ['one', 'two', 'three']
+
+var reversed = array1.reverse(); 
+console.log('reversed: ', reversed);
+// expected output: Array ['three', 'two', 'one']
+
+/* Careful: reverse is destructive. It also changes
+the original array */ 
+console.log('array1: ', array1);
+// expected output: Array ['three', 'two', 'one']
