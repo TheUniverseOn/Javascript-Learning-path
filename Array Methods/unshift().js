4bb215373c36b97be1e49d7b165b1104a2a8f901
Sophonias Isaac <sophonias8@gmail.com>
2019-09-28 21:34:51 +0100

    Add files via upload

--- Array Methods/unshift().js
+++ Array Methods/unshift().js
@@ -1,4 +1,6 @@
-//add to the front of an array 
-var fruits = [ 'Apple', 'Banana', 'kiwi', 'Mango', 'Pineapple', 'Grapes'] 
-var addFront = fruits.unshift('blackberry');
-console.log(fruits) /* [ 'blackberry', 'Banana', 'kiwi', 'Mango', 'Pineapple', 'Grapes' ]*/
+//The unshift(0 methods adds one or more elements to the begining of an Array and returns the new 
+// length of the array)
+
+var array1 = [1, 2, 3];
+console.log(array1.unshift(4, 5))
+console.log(array1)
