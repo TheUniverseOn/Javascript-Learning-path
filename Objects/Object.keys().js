098bf037b81148ce23720163a1c9957eb21347c8
Sophonias Isaac <sophonias8@gmail.com>
2019-09-30 20:04:33 +0100

    Add files via upload

--- Objects/Object.keys().js
+++ Objects/Object.keys().js
@@ -10,3 +10,24 @@ const employees = {
 
 const keys = Object.keys(employees) 
 console.log(keys); //[ 'boss', 'secretary', 'sales', 'accountant' ] 
+
+//Object.keys can be used to iterate through the keys and values of an object. 
+
+
+//iterate through the keys
+Object.keys(employees).forEach(key => {
+    let value = employees[key]; 
+
+    console.log( `${key}: ${value}`);
+}); 
+// boss: Michael
+// secretary: Pam
+// sales: Jim
+// accountant: Oscar
+
+
+// Object.keys is also useful for checking the lenght of an object. 
+// Get the length of the keys
+const length = Object.keys(employees).length;
+
+console.log(length); //4
