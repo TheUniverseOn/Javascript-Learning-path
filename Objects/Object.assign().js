6e84b368fffc939d9bde5bdc6315ffc14a67d777
Sophonias Isaac <sophonias8@gmail.com>
2019-10-01 08:00:36 +0100

    Update Object.assign().js

--- Objects/Object.assign().js
+++ Objects/Object.assign().js
@@ -18,3 +18,27 @@ console.log(character)
   Location: 'London', 
   job: 'Programmer and Entrepreuner', 
   programmingLang: 'Javascript' }  */
+
+
+
+  /*It is also possible to use the spread operator(...) to accomplish the same 
+  task in the code below, we'll modify how we declare the character through merging the 
+  name and details objects*/
+
+  //Initilise an object 
+
+  const name = {
+    firstName : 'Sophonias', 
+    Location : 'London'
+}; 
+//initialise another details 
+const details = {
+job: 'Programmer and Entrepreuner', 
+programmingLang: 'Javascript'
+
+}; 
+
+const character = {...name, ...details}
+console.log(character)
+
+//The spread synatc in object is also known as shallow-cloning.
