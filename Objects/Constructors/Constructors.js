27a3288f53b12cdaeafc254b13c1a61dc8778ff4
Sophonias Isaac <sophonias8@gmail.com>
2019-10-03 17:44:04 +0100

    Update Constructors.js

--- Objects/Constructors/Constructors.js
+++ Objects/Constructors/Constructors.js
@@ -43,4 +43,45 @@ console.log(Birdie)//Bird { name: 'Einstein', color: 'blue', numLegs: 2 } 
  
  
  
-//Extending Constrcutors to Receive Arguments 
+
+/**Extending Constrcutors to Receive Arguments 
+ * 
+ * The Bird constructor from the above example worked well. However, notice that all 
+ * Birds that are created with the Bird consstructor are automatically named 'Albert', are blue, and have two legs 
+ * > what if you want birda with different values for name and color? It's possible to chnage the properties of each bird manually but that
+ * would be a lot of work
+ * 
+ * 
+ */
+
+ let swan = new Bird(); 
+ swan.name = "Pino"; 
+ swan.color = 'white'; 
+
+ /** Suppose you were writing a program to keep track of hundreds or even \
+  * thousands of different birds in an aviary. It would take alot of 
+  * time to create all the birds, then change the properties to different values 
+  * for every one
+  * 
+  * To more easily create different Bird Objects, you can design your Bird 
+  * constrcutor to accept parameters */
+ function Bird(name, color){
+     this.name = name; 
+     this.color = color; 
+     this.numLegs = 2; 
+ } 
+
+
+ //Then pass in the values as arguments t degineeach unique bird into the Bird constructor; 
+ let cardinal = new Bird('Cardie', 'red'); 
+//This gives a new instance of Bird with name and color properties set to 
+//Cardie' and Red;. the numLegs  property is still set to 2. 
+
+// The cardinal has these properties 
+console.log(cardinal)//Bird { name: 'Cardie', color: 'red', numLegs: 2 } 
+
+/** The constrcutor is more flexible. It's now posssible to define the properties for each Bird at the time it 
+ * is created, which is one way that Javascript constrcutors are so useful. They group objects together based
+ * on shared characteristics and behaviour and define a blue print that automates their creation
+  */
+
