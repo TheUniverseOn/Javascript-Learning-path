689ce2fa48b70f9c989523a9277d9f46c867d7b6
Sophonias Isaac <sophonias8@gmail.com>
2019-10-05 20:07:47 +0100

    Update this.js

--- Expression operators/this.js
+++ Expression operators/this.js
@@ -29,68 +29,9 @@ var me = {
 var you = {
     name: "Reader"
 }; 
-console.log(identify.call(me));//kyle
+console.log(identify.call(me));//Sophonias
 console.log(identify.call(you)); //reader
 
 console.log(speak.call(me)); 
 console.log(speak.call(you));
 
-/** This code snippet allows the identify() and speak() functions to be reused against 
- * mulitple context objects(me and you), rather than needing a separate multiple context objects(me and you), 
- * rather than needing a separate version of the function for each object. 
- * 
- * instead of relying on this, you could have explicitl ypassed in a context object to both identify() and speak()
- */
-
-
-
-
- function foo(num){
-     console.log('foo:' + num );
-
-     this.count++; 
- }
-
- foo.count = 0; 
-
- var i; 
- for( i =0; i<10; i++){
-     if(i > 5){
-         foo (i); 
-     }
- }
-
- console.log(foo.count); 
-
-
- const   Phonecost = 600;
- const tax = 0.08; 
-var  accessory = 10
-const SpendingRestriction = 700
-
-var bankFund = 1000; 
-var amount = 0; 
-
-
-function calculateTax(amount){
- return amount * tax;  
-    
- }
-
- function formatAmount(amount){
-     return "$" + amount.toFixed(2); 
- }
- //keep buying while you still have money
-while(amount <bankFund){
-    amount = amount + Phonecost; 
-}
-
-//can we afford accessory?
-if(amount < SpendingRestriction){
-    amount = amount + accessory; 
-}
-
-amount = amount + calculateTax(amount); 
-console.log('my purchase' + formatAmount(amount))
-
-if (amount > bankFund) {    console.log("You can't afford this purchase. :("    );}
