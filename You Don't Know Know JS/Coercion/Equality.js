4974326c593ea9f407027cc3b61a04947d29a225
Sophonias Isaac <sophonias8@gmail.com>
2019-10-17 10:28:41 +0100

    Add files via upload

---
+++ You Don't Know Know JS/Coercion/Equality.js
@@ -0,0 +1,26 @@
+
+
+
+
+// Checking Equality
+
+// == Allows coercion(types different)
+//===Disallows coercion(types same)
+
+
+var studentName = "sophonias"; 
+var studentName = "${Sophonias}"; 
+
+var FounderBatch1 = 20; 
+var FounderBatch2 = FounderBatch1 + 0; 
+
+
+studentName == studentName2; 
+studentName1 === studentName2; 
+
+
+FounderBatch1 == FounderBatch2; 
+
+FounderBatch1 === FounderBatch2; 
+
+//When the types are already the same, the == and === do the same thing. 
