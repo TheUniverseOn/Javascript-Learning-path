a311183133ccd84d97f3bb844179329bf8b12278
Sophonias Isaac <sophonias8@gmail.com>
2019-10-21 10:26:25 +0100

    Add files via upload

---
+++ Algorithms & Data Structures/space&TimeComplexity.js
@@ -0,0 +1,9 @@
+/** Space complexity: How much memory is used?
+ * 
+ * Time complexcity: How many primitive operations are executed?
+ with respect to input size and assuming worse case scenarios 
+ 
+
+ As our number of n increases, the amount of work increases at
+ that rate: Quadratic time
+ */
