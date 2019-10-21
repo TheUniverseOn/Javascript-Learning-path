d84e083a9d584283117c754708425922720e6bda
Sophonias Isaac <sophonias8@gmail.com>
2019-10-21 14:26:36 +0100

    Create Recursion.js

---
+++ Algorithms & Data Structures/Recursion.js
@@ -0,0 +1,10 @@
+Recursion is when you define something in terms of itself. I.e What is a human being? A human is a being. That would be a 
+recurive defintion. 
+In Computer Science Recursive is when a function calls itself. Altough, it maintains the state at different levels of recursion,
+it also has a disadvantage that inherently leaves a large footprint as every function  call adds another call to the stack. Therefore,
+a better way to solve some problems can be done through loops(more complicated) rather than recursion. However, readability can
+be favorable than performance so recursion can also make sense to use. Always favour readability than performance. 
+
+
+
+Fibonacci is a good use case for Recursion, as the rule of fibonacci is adding to two previous terms together. 
