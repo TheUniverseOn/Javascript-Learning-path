bdb52f77b44b9a0401409a6beca54601836b4955
Sophonias Isaac <sophonias8@gmail.com>
2019-10-21 10:29:14 +0100

    Update space&TimeComplexity.js

--- Algorithms & Data Structures/space&TimeComplexity.js
+++ Algorithms & Data Structures/space&TimeComplexity.js
@@ -3,7 +3,19 @@
  * Time complexcity: How many primitive operations are executed?
  with respect to input size and assuming worse case scenarios 
  
+ 
+ Time and Space Complexity
 
- As our number of n increases, the amount of work increases at
- that rate: Quadratic time
+
+0(1) Constant time 
+Time is always constant regardless of the size of the input 
+
+0(n) Linear Time 
+Time is linear according to the size of the data. Time will grow in direction 
+proportion to the size of the input data. 
+
+0(log(n))  Logarithmic time: 
+
  */
+
+
