f91d94744b7daffbb15f5fb79e71c9465a28a175
Sophonias Isaac <sophonias8@gmail.com>
2019-10-23 12:01:30 +0100

    Update BubbleSort.js

--- Algorithms & Data Structures/BubbleSort.js
+++ Algorithms & Data Structures/BubbleSort.js
@@ -1,7 +1,31 @@
-Bubble sort- Algorithm, 
+// Bubble sort- Algorithm, 
 
-It's simple and good for small datasets. 
+// It's simple and good for small datasets. 
 
-Time - Worse case: O(n^2)
-Time - Best case: O(n)
+// Time - Worse case: O(n^2)
+// Time - Best case: O(n)
 
+function bubbleSort(arr){
+    const loop = arr.length; 
+
+    //loop for loop length
+    for(let i = 0; i < loop; i++){
+    
+        //cycle through arr items 
+        for(let j = 0; j < loop; j++){
+
+        //compare adjecent items 
+     
+        if(arr[j] > arr[j+1]) {
+            //swap
+            let temp = arr[j]; 
+            arr[j] = arr[j+1]; 
+            arr[j+1] = temp; 
+        }
+    }
+    }
+    console.log(arr)
+    return arr; 
+    }
+    
+bubbleSort([14, 33, 27, 35, 10, 56, 101 ]); //[ 10, 14, 27, 33, 35, 56, 101 ]
