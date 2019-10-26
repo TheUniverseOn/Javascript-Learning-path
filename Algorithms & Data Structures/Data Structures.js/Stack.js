624e3276a79fbf490deb6bc3dfac9ba1bf8a1b29
Sophonias Isaac <sophonias8@gmail.com>
2019-10-26 19:49:59 +0100

    Create Stack.js

---
+++ Algorithms & Data Structures/Data Structures.js/Stack.js
@@ -0,0 +1,29 @@
+/** STACKS
+ * 
+ * functions: push, pop, peek, length
+*/
+
+
+//basic usage of stack using array 
+var letters = []; 
+
+var word = 'racecar'
+var reverseWord = ''; 
+
+//put letters of word into stack
+
+
+for(var i = 0; i < word.length; i++){
+letters.push(word[i])
+}
+
+for(var i = 0; i < word.length; i++){
+    reverseWord += letters.pop(); 
+}
+
+if(reverseWord === word){
+    console.log(word + " is a palindrome") //racecar is a palindrome 
+} else {
+    console.log(word + " is not a palindrome");
+}
+
