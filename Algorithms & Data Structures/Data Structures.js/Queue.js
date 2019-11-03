0650e34f4032babb933b267c3b3134dac1c4f8c2
Sophonias Isaac <sophonias8@gmail.com>
2019-11-03 20:27:08 +0000

    Update Queue.js

--- Algorithms & Data Structures/Data Structures.js/Queue.js
+++ Algorithms & Data Structures/Data Structures.js/Queue.js
@@ -1,1 +1,2 @@
-A queue is a container of objects (a linear collection) that are inserted and removed according to the first-in first-out (FIFO) principle
+//A queue is a container of objects (a linear collection) that are inserted and removed
+//according to the first-in first-out (FIFO) principle
