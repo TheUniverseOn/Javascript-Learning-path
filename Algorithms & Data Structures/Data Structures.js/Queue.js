d1a926e6c09a76f603a6eeb797b3f075ed494b15
Sophonias Isaac <sophonias8@gmail.com>
2019-11-03 20:32:23 +0000

    Update Queue.js

--- Algorithms & Data Structures/Data Structures.js/Queue.js
+++ Algorithms & Data Structures/Data Structures.js/Queue.js
@@ -1,2 +1,8 @@
 //A queue is a container of objects (a linear collection) that are inserted and removed
 //according to the first-in first-out (FIFO) principle
+/* Queue methods 
+
+enqueue 
+dequeue 
+front
+isEmpty*/
