51b41e478e6010ceea1df280457512fa9813b6ff
Sophonias Isaac <sophonias8@gmail.com>
2019-11-04 18:09:48 +0000

    Update Override Inherited prototype methods.js

--- Objects/Override Inherited prototype methods.js
+++ Objects/Override Inherited prototype methods.js
@@ -12,3 +12,40 @@ Bird.prototype = Object.create(Animal.prototype);
 Bird.prototype.eat = function(){
 return 'peck peck'
 }
+/*As you can see behavior is shared through inheritance. However, there are cases when inheritance 
+is not the best solution. Inheritance does not work well for unrelated objects like Bird and Airplane They can 
+fly both, but is not a type of Airplane and vice versa
+
+For unrelated objects, it's better to use mixins. A mixin allows other objects to use a collection of function 
+* /
+
+//mixin 
+let flymixin = function(obj){
+    obj.fly = function(){
+        return ('Flying, woosh')
+    }
+}
+
+//the flymixin takes any object and gives it the fly method
+
+let bird = {
+    name: "Donald", 
+    numLegs: 2
+};
+
+let plane = {
+    model: '777', 
+    numPassenegers: 500
+}; 
+//below bird and plane are passed into flymixin, which then 
+//assigns the fly function to each object. Now bird and plane can 
+//both fly; 
+flymixin(bird)
+
+flymixin(plane)
+
+console.log(bird.fly())//Flying, woosh
+console.log(plane.fly())//Flying, woosh
+
+//Note how the mixin allows for the same fly methods to be 
+//reused by unrelated objects bird and plane 
