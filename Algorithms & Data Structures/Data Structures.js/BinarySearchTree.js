b7192cf34a788970382717770554fa25e2f6d53f
Sophonias Isaac <sophonias8@gmail.com>
2019-11-06 13:11:20 +0000

    Create BinarySearchTree.js

---
+++ Algorithms & Data Structures/Data Structures.js/BinarySearchTree.js
@@ -0,0 +1,5 @@
+/*Binary Search Tree 🌲 
+
+ A node in BST has zero, one or two subtrees/ children. Every element in the left subtree Is lesser
+ than the value of the node and every element in the right is greater than the value of the node.
+ Like linked lists we just have to change the pointers when we add new elements */ 
