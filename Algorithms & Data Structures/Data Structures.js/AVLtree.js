9eb3b02eb0498bd7ee6a4229b8309a75e574de99
Sophonias Isaac <sophonias8@gmail.com>
2019-11-06 17:01:29 +0000

    Update AVLtree.js

--- Algorithms & Data Structures/Data Structures.js/AVLtree.js
+++ Algorithms & Data Structures/Data Structures.js/AVLtree.js
@@ -52,10 +52,101 @@ left node are nodes who have no children
 single rotation 
              8  node A
             /  \ 
- node C  5       9 node B 
-       
+ node C  5       9 node /
+
+
 
 */
 
 
+
+/*double rotation
+
+
+ */ 
+class Tree { 
+constructor() {
+this.root = null; 
+       
+}
+add(value) {
+if(!this.root) {
+this.root = new Node(value);
+
+} else {
+this.root.add(value) 
+
+}
+}
+toObject(){
+return this.root;
+}
+
+}
+
+
+class Node {
+constructor(value, left = null, right= null) {
+this.value = value;
+this.left = left;
+ this.right = right;  
+   this.height = 1; 
+       
+
+}
+       
+       add(value) {
+
+if(value < this.value){
+// go left 
+if(this.left) {
+this.left.add(value);
+}
+       else {
+this.left = new Node(value); 
+       } 
+       // making sure height stats the same
+       if(!this.right || this.right.height < this.left.height){
+this.height = this.left.height + 1;
+
+}
+}
+else {
+if(this.right) {
+this.right.add(value);
+}
+       else { 
+this.right = new Node(value);
+}
+     if(!this.left || this.right.height > this.left.height)  {
+this.height = this.right.height + 1;
+
+
+} 
+       
+}
+              this.balance()
+}
+       balance(){
+const rightHeight = (this.right) ? this.right.height : 0;
+  const leftHeight = (this.left) ? this.left.height : 0;           
+
+if(leftHeight > rightHeight + 1) { 
+const leftRightHeight = (this.left.right) ? this.left.right.height : 0;
+ const leftLeftHeight = (this.left.left) ? this.left.left.height : 0;
+       
+       if(leftRightHeight > leftLeftHeight){
+this.left.rotateRR();
+
+
+}
+       
+       this.rotateLL();
+}
+              else {
+const rightRight
+}
+ } 
+       
+}
 
