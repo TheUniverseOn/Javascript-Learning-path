f0188af50bd17133c8b02e607e2eb3342721acd5
Sophonias Isaac <sophonias8@gmail.com>
2019-11-06 15:22:34 +0000

    Update AVLtree.js

--- Algorithms & Data Structures/Data Structures.js/AVLtree.js
+++ Algorithms & Data Structures/Data Structures.js/AVLtree.js
@@ -14,5 +14,38 @@ The benefit of all this extra effort is we can now guarntee that we won't hit
 those bad or worse case scenerios of having greatly out of balance trees
 and guarantee we won't hit the O(n) cases. our worst cases becomes O(log n)
 
+       5
+        \
+          8
+  -> currenlty valid AVL tree 
+  -> .add called with 9
+  
+  5 - node A
+   \ 
+    8 - node B 
+      \ 
+       9 - node C 
+( on the way up from the recursion) 
+-> check balance of node C: left height is 0, right height is 0, balanced
+-> check balance of node B: Left height is 0, right height is 1, balanced
+-> check balance of Node A: left height is 0: right height is 2: unbalanced, 
+right heavy: child is right heavy.
+
+
+
+
+-> perform right rotation 
+-> swap the value of nodes A and B 
+->make node B the left child of node A
+->make node C the right child of node A
+-> move node C the right child to its left child
+(in the case they're both null) 
+->make node A's  original left child 
+( which was null in this case) the left child of node B 
+->UPDATe the heights of all the nodes involved 
+
 
 */
+
+
+
