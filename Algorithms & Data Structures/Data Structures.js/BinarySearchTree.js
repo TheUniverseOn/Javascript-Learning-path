fb9029dbabac0eec91c0e6084022a55c8170b030
Sophonias Isaac <sophonias8@gmail.com>
2019-11-06 13:16:20 +0000

    Update BinarySearchTree.js

--- Algorithms & Data Structures/Data Structures.js/BinarySearchTree.js
+++ Algorithms & Data Structures/Data Structures.js/BinarySearchTree.js
@@ -3,3 +3,7 @@
  A node in BST has zero, one or two subtrees/ children. Every element in the left subtree Is lesser
  than the value of the node and every element in the right is greater than the value of the node.
  Like linked lists we just have to change the pointers when we add new elements */ 
+
+
+//Best get an average case of o(log n) in gets, adds and deletes, but they can have a worst case of
+//o(n) if you do something like add a sorted list to a BST.
