355ff11859ce47f9ac8e697067c14b45b9d76640
Sophonias Isaac <sophonias8@gmail.com>
2019-11-09 16:53:37 +0000

    Create Sets.js

---
+++ Algorithms & Data Structures/Data Structures.js/Sets.js
@@ -0,0 +1,16 @@
+/* A set is a collection of items that are unordered and consists of unique
+elements(meaning they cannnot be repeated). This data structure uses the same math concept as 
+finite sets but applied  to a  computer science data structure 
+
+let's take a look at the math concept  of sets  before we dive into the compuuter 
+science implementation of it. In mathematics, a set is a collection pd distinict objects 
+
+For example, we have a set of natural numbers, which consists of integer numbers greater than 
+or equal to 0-that  is N  = {0, 1,2,3,4,5,6, ...} The list of the objects 
+within the set is surrounded by {} currely braces 
+
+There is also the null set concept. A set with no element is called a null set or 
+or an empty set. An example would be a set of prime numbers between 24 and 29. as there 
+is mo prime number( a natural number greater than 1 that has no positive divisors other than 
+1 and itself) between 24 /!; 29, the set will be empty. we will represent an empty
+set with {}*/
