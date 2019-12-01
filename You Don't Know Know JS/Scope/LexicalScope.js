5149bdc98e31fd9f192e4f9593b4453a173279d0
Sophonias Isaac <sophonias8@gmail.com>
2019-12-01 11:06:09 +0000

    Create LexicalScope.js

---
+++ You Don't Know Know JS/Scope/LexicalScope.js
@@ -0,0 +1,16 @@
+// var student = "sophonias"; 
+
+// function otherClass(){
+// var student = "me"; 
+
+// function ask(question){
+// console.log(student, question); //me why?
+// }
+
+// ask("why?"); 
+// }
+//  otherClass();
+/** Lexical scope: console.log(student) is decided at compile time */
+
+
+//dynamic scope doesn't exist in javascript
