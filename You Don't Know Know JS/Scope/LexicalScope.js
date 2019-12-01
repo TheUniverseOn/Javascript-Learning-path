8c611239ca891f5177bd2e8c9a0d4bb1fd6e5e10
Sophonias Isaac <sophonias8@gmail.com>
2019-12-01 11:28:10 +0000

    Update LexicalScope.js

--- You Don't Know Know JS/Scope/LexicalScope.js
+++ You Don't Know Know JS/Scope/LexicalScope.js
@@ -27,3 +27,24 @@ function otherClass(){
     }
     
 otherClass(); 
+
+
+//fixing the above collison by putting them in different buckets by wrapping a function around it
+
+var teacher = 'kyle simpson'; 
+
+
+function anotherTeacher(){
+
+    var teacher = 'Brian Holt';
+    console.log(teacher); //Brian holt
+}
+anotherTeacher()
+console.log(teacher)//kyle simpson 
+//Even though there is a new scope to deal with the naming problem, we still have a name collision problem. 
+/** The princple of least exposure says: you should default  to keeping everything private, and only exposing the
+minimal neccessary. 
+it 
+1. avoid naming collision
+2.Hiding something means: people can misuse it 
+3. you also protect yourself from future refactoring */
