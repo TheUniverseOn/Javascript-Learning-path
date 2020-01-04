eebe9bd5080b01c02bca802e162be1a3960bb137
Sophonias Isaac <sophonias8@gmail.com>
2020-01-04 18:13:21 +0000

    Create ImmutableObject.js

---
+++ Objects/ImmutableObject.js
@@ -0,0 +1,6 @@
+
+accomplishing immutability with objects 
+
+1. writable attribute of properties
+2. Sealing an object(you can't add or delete but you cwn change) 
+3. Freezing an object( you can't change or delete properties that are already ther)e
