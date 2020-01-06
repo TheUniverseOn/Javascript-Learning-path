3b49deb0e21b67c5cb4050d5ef7e542bb0a610fa
Sophonias Isaac <sophonias8@gmail.com>
2020-01-06 20:34:49 +0000

    Update ThinkingLikeaProgrammer.js

--- ThinkingLikeaProgrammer.js
+++ ThinkingLikeaProgrammer.js
@@ -1,7 +1,11 @@
-Avoiding declaring  global variables a lot. 
+Avoide declaring  global variables a lot. 
  
 Global variables can changed anywhere. 
 Global variables pollute the global namespace shared by everyone. 
 this can cause collisions. 
 Reliance on global variables avoids better programming patterns. 
 
+
+
+2. module pattern 
+breaking code part in discreet elements
