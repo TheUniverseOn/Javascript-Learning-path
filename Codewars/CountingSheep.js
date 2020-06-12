6de907871db313ff6c9b463483c32c41a91bc63f
Sophonias Isaac <sophonias8@gmail.com>
2020-06-12 17:14:46 +0100

    Create CountingSheep.js

---
+++ Codewars/CountingSheep.js
@@ -0,0 +1,25 @@
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+
+
+const countSheeps = (arrayOfSheep) => arrayOfSheep.filter(item => item === true).length
+
+var array1 = [true,  true,  true,  false,
+              true,  true,  true,  true ,
+              true,  false, true,  false,
+              true,  false, false, true ,
+              true,  true,  true,  true ,
+              false, false, true,  true ];
+              
+Test.expect(countSheeps(array1) == 17, "There are 17 sheeps in total")
