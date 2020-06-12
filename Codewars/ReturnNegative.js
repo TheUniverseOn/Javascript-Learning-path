69540c61da451f073d123b13402267c8d98d700c
Sophonias Isaac <sophonias8@gmail.com>
2020-06-12 14:59:50 +0100

    Create ReturnNegative.js

---
+++ Codewars/ReturnNegative.js
@@ -0,0 +1,6 @@
+In this simple assignment you are given a number and have to make it negative.
+But maybe the number is already negative?
+
+const  makeNegative=(num) => num > 0 ? - + num : num
+
+Test.assertEquals(makeNegative(42), -42);
