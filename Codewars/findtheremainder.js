80aeea7ffe22251f72c383d40c96591b544c5191
Sophonias Isaac <sophonias8@gmail.com>
2020-06-12 14:02:00 +0100

    Update findtheremainder.js

--- Codewars/findtheremainder.js
+++ Codewars/findtheremainder.js
@@ -1,11 +1,11 @@
 
 
-Write a function that accepts two arguments and returns the remainder after dividing 
-the larger number by the smaller number. Division by zero should return NaN 
-(in C#, throw a new DivideByZeroException instead). Arguments will both be integers.
+// Write a function that accepts two arguments and returns the remainder after dividing 
+// the larger number by the smaller number. Division by zero should return NaN 
+// (in C#, throw a new DivideByZeroException instead). Arguments will both be integers.
 
 
-
+//answer
 const remainder = (a, b) => (a > b ? a % b : b  % a)
 
 Test.describe('Remainder Function', function(){
