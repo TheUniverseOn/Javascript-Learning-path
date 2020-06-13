cdb0781cbb6c5c905a1b10a2f74f9008d0797cd1
Sophonias Isaac <sophonias8@gmail.com>
2020-06-13 14:02:38 +0100

    Create BasicMathematicalOperations.js

---
+++ BasicMathematicalOperations.js
@@ -0,0 +1,25 @@
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples
+basicOp('+', 4, 7)         // Output: 11
+basicOp('-', 15, 18)       // Output: -3
+basicOp('*', 5, 5)         // Output: 25
+basicOp('/', 49, 7)        // Output: 7
+
+
+
+const basicOp=(operation, value1, value2)=>{
+return operation === '-' ? value1-value2 : 
+operation === '+' ? value1 + value2 : operation === '*' ? value1 * value2 : operation === '/' ? value1 / value2 : 0
+
+}
+
+
+console.log("Basic tests\n");
+Test.assertSimilar(basicOp('+', 4, 7), 11);
+Test.assertSimilar(basicOp('-', 15, 18), -3);
+Test.assertSimilar(basicOp('*', 5, 5), 25);
+Test.assertSimilar(basicOp('/', 49, 7), 7);
