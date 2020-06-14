0699e34912563cf65f880e82cc1e8b5eefcd1a4b
Sophonias Isaac <sophonias8@gmail.com>
2020-06-14 14:51:07 +0100

    Create Invert Values.js

---
+++ Codewars/Invert Values.js
@@ -0,0 +1,19 @@
+iven a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list.
+
+
+//solution 
+const invert = (array) = array.map( i => i - (i*2))
+
+describe("Invert array values", function(){
+  it("Basic Tests", function(){
+    Test.assertDeepEquals(invert([1,2,3,4,5]), [-1,-2,-3,-4,-5]);
+    Test.assertDeepEquals(invert([1,-2,3,-4,5]), [-1,2,-3,4,-5]);
+    Test.assertDeepEquals(invert([]), []);
+    Test.assertDeepEquals(invert([0]), [0]);
+  });
+});
