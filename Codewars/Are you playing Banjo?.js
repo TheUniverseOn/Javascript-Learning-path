7b6b9e00d7fc7c3b3327b1f763283037f2940256
Sophonias Isaac <sophonias8@gmail.com>
2020-06-14 19:03:21 +0100

    Create Are you playing Banjo?.js

---
+++ Codewars/Are you playing Banjo?.js
@@ -0,0 +1,19 @@
+
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings.
+
+
+
+const areYouPlayingBanjo = (name) => name.includes('R' || 'r') ? `${name} plays banjo` : `${name} does not play banjo`
+
+Test.assertEquals(areYouPlayingBanjo("Martin"), "Martin does not play banjo");
+Test.assertEquals(areYouPlayingBanjo("Rikke"), "Rikke plays banjo");
+
+
+
