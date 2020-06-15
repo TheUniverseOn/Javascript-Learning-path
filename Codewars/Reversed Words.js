413bba887a0ce0faf22ae2bec83cc3baa9a1158c
Sophonias Isaac <sophonias8@gmail.com>
2020-06-15 10:58:15 +0100

    Create Reversed Words.js

---
+++ Codewars/Reversed Words.js
@@ -0,0 +1,20 @@
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Example:
+
+reverseWords("The greatest victory is that which requires no battle")
+// should return "battle no requires which that is victory greatest The"
+
+//solution 
+const reversedWords=(str) => str.split(' ').reverse().join(' ') 
+
+
+describe("reverseWords",function(){
+  it("should work for some examples", function(){
+    Test.assertEquals(reverseWords("hello world!"), "world! hello")
+    Test.assertEquals(reverseWords("yoda doesn't speak like this" ),  "this like speak doesn't yoda")
+    Test.assertEquals(reverseWords("foobar"                       ),  "foobar")
+    Test.assertEquals(reverseWords("kata editor"                  ),  "editor kata")
+    Test.assertEquals(reverseWords("row row row your boat"        ),  "boat your row row row")
+  });
+});
