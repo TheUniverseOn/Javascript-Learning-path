ce5304110a3fe017cb51ccc8eed8089dd72d37fe
Sophonias Isaac <sophonias8@gmail.com>
2020-06-15 15:45:32 +0100

    Create Get Planet Name by ID.js

---
+++ Codewars/Get Planet Name by ID.js
@@ -0,0 +1,43 @@
+The function is not returning the correct values. Can you figure out why?
+
+getPlanetName(3); // should return 'Earth'
+
+function getPlanetName(id){
+var name; 
+switch(id){
+case 1: 
+name = 'Mercury'
+break; 
+case 2: 
+name = 'Venus'
+break; 
+case 3: 
+name = 'Earth'
+break; 
+case 4: 
+name= 'Mars'
+break; 
+case 5: 
+name = 'Jupiter'
+break; 
+case 6: 
+name = 'Saturn'
+break; 
+case 7: 
+name = 'Uranus'
+break; 
+
+case 8: 
+name = 'Neptune'
+
+default;
+}
+
+return name
+
+
+}
+
+Test.assertEquals(getPlanetName(2), 'Venus');
+Test.assertEquals(getPlanetName(5), 'Jupiter');
+Test.assertEquals(getPlanetName(3), 'Earth');
