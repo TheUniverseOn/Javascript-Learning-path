0650b693feb8f4323936cc438e88f1acbe2dcefc
Sophonias Isaac <sophonias8@gmail.com>
2020-06-15 12:40:51 +0100

    Create You only need one - Beginner.js

---
+++ You only need one - Beginner.js
@@ -0,0 +1,20 @@
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+
+
+//solution 
+
+const check = (a, x) => a.includes(x) ? true : false 
+
+
+
+
+Test.describe("Basic tests",_=>{
+Test.assertEquals(check([66, 101], 66), true);
+Test.assertEquals(check([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
+Test.assertEquals(check(['t', 'e', 's', 't'], 'e'), true);
+Test.assertEquals(check(['what', 'a', 'great', 'kata'], 'kat'), false);
+})
